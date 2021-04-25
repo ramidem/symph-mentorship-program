cf346d71e91456a63b8776604bd96be794351317
Med Aduh <med@symph.co>
2021-04-26 02:55:47 +0800

    [Feature][Med] update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,26 @@
+import styled from 'styled-components';
+
 function Footer() {
   return (
-    <footer>
+    <FooterWrapper>
       <span>Made with ❤️ by Symph.</span>
-      <span>© 2021 Symph. All Rights Reserved.</span>
-    </footer>
+      <br />
+      <span className="copyright">© 2021 Symph. All Rights Reserved.</span>
+    </FooterWrapper>
   );
 }
 
 export default Footer;
+
+const FooterWrapper = styled.footer`
+  margin-bottom: 70px;
+  margin-top: 120px;
+  text-align: center;
+
+  span.copyright {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 31px;
+    color: #9c9c9c;
+  }
+`;
